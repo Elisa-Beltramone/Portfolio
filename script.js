95ea6dafd107cd8bd3c572761eda95c4d82b6ce8
Elisa <elibeltramone@gmail.com>
2024-05-08 08:58:51 +0200

    cursor

--- script.js
+++ script.js
@@ -21,3 +21,17 @@ document.addEventListener("DOMContentLoaded", function() {
         starsContainer.appendChild(star);
     }
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+
+const cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", (e) => {
+            let x = e.pageX;
+            let y = e.pageY;
+
+            cursor.style.top = y + "px";
+            cursor.style.left = x + "px";
+} ); 
+} )
+
